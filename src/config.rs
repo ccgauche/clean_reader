010fa58d0be639607ed63961be5515c2aea433bb
ccgauche <gaucheron.laurent@gmail.com>
2021-11-21 12:13:20 +0100

    Changed things in extension + added default config + Rustls switch

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use once_cell::sync::Lazy;
 
 #[derive(serde::Deserialize)]
@@ -12,5 +14,12 @@ pub struct Config {
     pub max_size: u64,
 }
 
-pub static CONFIG: Lazy<Config> =
-    Lazy::new(|| toml::from_str(&std::fs::read_to_string("config.toml").unwrap()).unwrap());
+pub static CONFIG: Lazy<Config> = Lazy::new(|| {
+    toml::from_str({
+        if !Path::new("config.toml").exists() {
+            std::fs::write("config.toml", include_str!("../config.toml")).unwrap();
+        }
+        &std::fs::read_to_string("config.toml").unwrap()
+    })
+    .unwrap()
+});
