8702e9dc883f3201fafba6733060bc5a9853a204
ccgauche <gaucheron.laurent@gmail.com>
2021-11-21 14:01:27 +0100

    Cleaned up html generation

--- src/text_element/compiler/html.rs
+++ src/text_element/compiler/html.rs
@@ -13,66 +13,59 @@ impl<'a> TextCompound<'a> {
                 string.push(' ');
             }
             TextCompound::Link(a, b) => {
-                string.push_str("<a href=\"");
-                if !ctx.download
-                    && !b.starts_with("mailto:")
-                    && !b.starts_with('#')
-                    && is_html(b.as_ref())
-                {
-                    string.push_str("/m/");
-                    string.push_str(&get_shortened_from_url(b));
-                } else {
-                    string.push_str(b);
-                }
-                string.push_str("\">");
-                a.html(ctx, string);
-                string.push_str("</a>");
+                push_html(
+                    string,
+                    "a",
+                    Some((
+                        "href".to_owned(),
+                        if !ctx.download
+                            && !b.starts_with("mailto:")
+                            && !b.starts_with('#')
+                            && is_html(b.as_ref())
+                        {
+                            format!("/m/{}", get_shortened_from_url(b))
+                        } else {
+                            b.to_string()
+                        },
+                    )),
+                    a,
+                    ctx,
+                );
             }
             TextCompound::Italic(a) => {
-                string.push_str("<i>");
-                a.html(ctx, string);
-                string.push_str("</i>");
+                push_simple_html(string, "i", a, ctx);
             }
             TextCompound::Bold(a) => {
-                string.push_str("<b>");
-                a.html(ctx, string);
-                string.push_str("</b>");
+                push_simple_html(string, "b", a, ctx);
             }
             TextCompound::Array(a) => a.iter().for_each(|x| x.html(ctx, string)),
             TextCompound::Abbr(a, b) => {
-                string.push_str("<abbr title=\"");
-                string.push_str(b);
-                string.push_str("\">");
-                a.html(ctx, string);
-                string.push_str("</abbr>");
+                push_html(string, "small", Some(("title", b.as_ref())), a, ctx);
             }
             TextCompound::Sup(a) => {
-                string.push_str("<sup>");
-                a.html(ctx, string);
-                string.push_str("</sup>");
+                push_simple_html(string, "sup", a, ctx);
             }
             TextCompound::Sub(a) => {
-                string.push_str("<sub>");
-                a.html(ctx, string);
-                string.push_str("</sub>");
+                push_simple_html(string, "sub", a, ctx);
             }
             TextCompound::Small(a) => {
-                string.push_str("<small>");
-                a.html(ctx, string);
-                string.push_str("</small>");
+                push_simple_html(string, "small", a, ctx);
             }
             TextCompound::Br => {
                 string.push_str("<br/>");
             }
             TextCompound::Code(a) => {
                 if a.contains('\n') {
-                    string.push_str("<pre><code>");
-                    string.push_str(&html_escape::encode_text(a));
-                    string.push_str("</code></pre>");
+                    push_simple(string, "pre", |string| {
+                        push_simple(string, "code", |string| {
+                            string.push_str(&html_escape::encode_text(a));
+                        });
+                    });
                 } else {
-                    string.push_str("<code>");
-                    string.push_str(&html_escape::encode_text(a));
-                    string.push_str("&nbsp;</code>");
+                    push_simple(string, "code", |string| {
+                        string.push_str(&html_escape::encode_text(a));
+                        string.push_str("&nbsp;")
+                    });
                 }
             }
 
@@ -88,59 +81,90 @@ impl<'a> TextCompound<'a> {
                     .map(|x| format!("#{}", x))
                     .collect();
                 let header = match a {
-                    Header::H1 => '1',
-                    Header::H2 => '2',
-                    Header::H3 => '3',
-                    Header::H4 => '4',
-                    Header::H5 => '5',
+                    Header::H1 => "h1",
+                    Header::H2 => "h2",
+                    Header::H3 => "h3",
+                    Header::H4 => "h4",
+                    Header::H5 => "h5",
                 };
 
-                string.push_str("<h");
-                string.push(header);
-                if !c.is_empty() {
-                    string.push_str(" id=\"");
-                    string.push_str(&c.join(" "));
-                    string.push('"');
-                }
-                string.push('>');
-                b.html(ctx, string);
-                string.push_str("</h");
-                string.push(header);
-                string.push('>');
+                push_html(
+                    string,
+                    header,
+                    if !c.is_empty() {
+                        Some(("id".to_string(), c.join(" ")))
+                    } else {
+                        None
+                    },
+                    b,
+                    ctx,
+                );
             }
             Self::Ul(a) => {
-                string.push_str("<ul>");
-                for x in a {
-                    string.push_str("<li>");
-                    x.html(ctx, string);
-                    string.push_str("</li>");
-                }
-                string.push_str("</ul>");
+                push_simple(string, "il", |string| {
+                    for x in a {
+                        push_simple_html(string, "li", x, ctx);
+                    }
+                });
             }
 
             Self::P(a) => {
-                string.push_str("<p>");
-                a.html(ctx, string);
-                string.push_str("</p>");
+                push_simple_html(string, "p", a, ctx);
             }
             Self::Table(a) => {
-                string.push_str("<table>");
-                for i in a {
-                    string.push_str("<tr>");
-                    for (a, b) in i {
-                        string.push_str(if *a { "<th>" } else { "<td>" });
-                        b.html(ctx, string);
-                        string.push_str(if *a { "</th>" } else { "</td>" });
+                push_simple(string, "table", |string| {
+                    for i in a {
+                        push_simple(string, "tr", |string| {
+                            for (a, b) in i {
+                                push_simple_html(string, if *a { "th" } else { "td" }, b, ctx);
+                            }
+                        });
                     }
-                    string.push_str("</tr>");
-                }
-                string.push_str("</table>");
+                });
             }
             Self::Quote(a) => {
-                string.push_str("<quote>");
-                a.html(ctx, string);
-                string.push_str("</quote>");
+                push_simple_html(string, "quote", a, ctx);
             }
         }
     }
+}
+
+fn push_simple_html(string: &mut String, a: &str, html: &TextCompound, ctx: &Context) {
+    push_html::<String>(string, a, None, html, ctx);
+}
+
+fn push_simple(string: &mut String, a: &str, f: impl FnOnce(&mut String)) {
+    push(string, a, None as Option<(String, String)>, f);
+}
+
+fn push<T: Into<String>>(
+    string: &mut String,
+    a: &str,
+    attribute: Option<(T, T)>,
+    f: impl FnOnce(&mut String),
+) {
+    string.push('<');
+    string.push_str(a);
+    if let Some((a, b)) = attribute {
+        string.push(' ');
+        string.push_str(&a.into());
+        string.push_str("=\"");
+        string.push_str(&b.into());
+        string.push('"');
+    }
+    string.push('>');
+    f(string);
+    string.push_str("</");
+    string.push_str(a);
+    string.push('>');
+}
+
+fn push_html<T: Into<String>>(
+    string: &mut String,
+    a: &str,
+    attribute: Option<(T, T)>,
+    html: &TextCompound,
+    ctx: &Context,
+) {
+    push(string, a, attribute, |string| html.html(ctx, string));
 }
